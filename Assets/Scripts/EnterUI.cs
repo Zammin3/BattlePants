70a610a2aaa8ca488563138ee778a74b8c154a7f
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-07-22 15:01:20 +0900

    enter scene

---
+++ Assets/Scripts/EnterUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EnterUI : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField nicknameInputField;
+    [SerializeField]
+    private GameObject enterRoomUI;
+
+    public void OnClickEnterGameButton()
+    {
+        if(nicknameInputField.text != "")
+        {
+            PlayerSettings.nickname = nicknameInputField.text; 
+        }
+        else
+        {
+            nicknameInputField.GetComponent<Animator>().SetTrigger("on");
+        }
+    }
+
+    public void CreateRoom()
+    {
+        if (nicknameInputField.text != "")
+        {
+            var manager = PantsRoomManager.singleton;
+
+            manager.StartHost();
+        }
+    }
+
+    public void EnterRoom()
+    {
+        if (nicknameInputField.text != "")
+        {
+            var manager = PantsRoomManager.singleton;
+
+            manager.StartClient();
+        }
+    }
+}
