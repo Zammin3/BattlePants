ace418b71bdec0cf11ac458e968edbc7dbb42442
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-07-26 00:34:39 +0900

    gameobject 정리

---
+++ Assets/Door.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void OpenDoor()
+    {
+        // 여기서 도어를 여는 작업을 수행합니다.
+        Debug.Log("Door opened!");
+    }
+}
--- Assets/Scripts/PantsGamePlayer.cs
+++ Assets/Scripts/PantsGamePlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PantsGamePlayer : MonoBehaviour
 {
+    public Door door; // Door 컴포넌트의 참조
+    private bool isInTrigger = false; // 플레이어가 도어 트리거 내에 있는지 표시
+
     private void Start()
     {
         Camera cam = Camera.main;
@@ -18,6 +21,24 @@ public class PantsGamePlayer : MonoBehaviour
         {
             gameObject.transform.position = new Vector3(20f, -6f, 0f);
         }
+
+        if (isInTrigger && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            door.OpenDoor(); // 도어를 엽니다
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == door.gameObject)
+            isInTrigger = true; // isInTrigger를 true로 설정
+    }
+
+    // 플레이어가 도어 트리거에서 나가면
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == door.gameObject)
+            isInTrigger = false; // isInTrigger를 false로 설정
     }
 
 }
